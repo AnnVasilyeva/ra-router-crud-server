3cf4c79dc522c5e0d99afaa8e90e0b853e1f9450
Ann Vasilyeva <skazkadreamanna@gmail.com>
2021-02-02 21:43:43 +0300

    change

--- post.js
+++ post.js
@@ -1,77 +1,77 @@
-const { v4: uuidv4 } = require("uuid");
-const fs = require("fs");
-const path = require("path");
-
-class Post {
-    constructor(content) {
-        this.id = uuidv4();
-        this.content = content;
-    }
-
-    async save() {
-        const tickets = await Post.getAll();
-        tickets.push(this.toJSON());
-        return  new Promise((resolve, reject) =>{
-            fs.writeFile(
-                path.join(__dirname, 'public', 'db.json'),
-                JSON.stringify(tickets),
-                err => {
-                    if(err){
-                        reject(err)
-                    }else{
-                        resolve()
-                    }
-
-                }
-            )
-        })
-
-    }
-
-    toJSON() {
-        return {
-            content: this.content,
-            id: this.id,
-        }
-    }
-
-    static getAll() {
-        return new Promise((resolve, reject) => {
-            fs.readFile(
-                path.join(__dirname, 'public', 'db.json'),
-                'utf-8',
-                (err, content) => {
-                    if (err) {
-                        reject(err)
-                    }else{
-                        resolve(JSON.parse(content))
-                    }
-                }
-            )
-        })
-    }
-
-    static async delete(id){
-        let tickets = await Post.getAll();
-        tickets = tickets.filter(elem => elem.id !== id);
-
-        return  new Promise((resolve, reject) =>{
-            fs.writeFile(
-                path.join(__dirname, 'public', 'db.json'),
-                JSON.stringify(tickets),
-                err => {
-                    if(err){
-                        reject(err)
-                    }else{
-                        resolve()
-                    }
-                }
-            )
-        })
-    }
-
-
-
-}
-
-module.exports = Post;
+// const { v4: uuidv4 } = require("uuid");
+// const fs = require("fs");
+// const path = require("path");
+//
+// class Post {
+//     constructor(content) {
+//         this.id = uuidv4();
+//         this.content = content;
+//     }
+//
+//     async save() {
+//         const tickets = await Post.getAll();
+//         tickets.push(this.toJSON());
+//         return  new Promise((resolve, reject) =>{
+//             fs.writeFile(
+//                 path.join(__dirname, 'public', 'db.json'),
+//                 JSON.stringify(tickets),
+//                 err => {
+//                     if(err){
+//                         reject(err)
+//                     }else{
+//                         resolve()
+//                     }
+//
+//                 }
+//             )
+//         })
+//
+//     }
+//
+//     toJSON() {
+//         return {
+//             content: this.content,
+//             id: this.id,
+//         }
+//     }
+//
+//     static getAll() {
+//         return new Promise((resolve, reject) => {
+//             fs.readFile(
+//                 path.join(__dirname, 'public', 'db.json'),
+//                 'utf-8',
+//                 (err, content) => {
+//                     if (err) {
+//                         reject(err)
+//                     }else{
+//                         resolve(JSON.parse(content))
+//                     }
+//                 }
+//             )
+//         })
+//     }
+//
+//     static async delete(id){
+//         let tickets = await Post.getAll();
+//         tickets = tickets.filter(elem => elem.id !== id);
+//
+//         return  new Promise((resolve, reject) =>{
+//             fs.writeFile(
+//                 path.join(__dirname, 'public', 'db.json'),
+//                 JSON.stringify(tickets),
+//                 err => {
+//                     if(err){
+//                         reject(err)
+//                     }else{
+//                         resolve()
+//                     }
+//                 }
+//             )
+//         })
+//     }
+//
+//
+//
+// }
+//
+// module.exports = Post;
--- server.js
+++ server.js
@@ -1,59 +1,56 @@
 const http = require('http');
-const path = require('path');
+// const path = require('path');
 const Koa = require('koa');
 const Router = require('koa-router');
-const Post = require('./post');
+// const Post = require('./post');
 const cors = require('koa2-cors');
 const koaBody = require('koa-body');
-const koaStatic = require('koa-static');
+// const koaStatic = require('koa-static');
 const app = new Koa();
 
-const public = path.join(__dirname, '/public');
+// const public = path.join(__dirname, '/public');
 
 app.use(cors());
-app.use(koaStatic(public));
+// app.use(koaStatic(public));
 
 app.use(koaBody({
     json: true,
-    urlencoded: true,
-    multipart: true,
-    text: true,
 }));
 
-// let posts = [{id: 0, content: 'Hello World!'}];
-// let nextId = 1;
+let posts = [{id: 0, content: 'Hello World!'}];
+let nextId = 1;
 
 const router = new Router();
 
 router.get('/posts', async (ctx, next) => {
-    ctx.response.body = Post.getAll();
+    ctx.response.body = posts;
 })
 
     .post('/posts', async(ctx, next) => {
-        const {content} = ctx.request.body;
+        const {content, id} = ctx.request.body;
 
-        const ticketPost = new Post(content);
-        await ticketPost.save();
-        ctx.response.body = ticketPost;
+        // const ticketPost = new Post(content);
+        // await ticketPost.save();
+        // ctx.response.body = ticketPost;
 
-        // if (id !== 0) {
-        //     posts = posts.map(o => o.id !== id ? o : {...o, content: content});
-        //     ctx.response.status = 204;
-        //     return;
-        // }
-        //
-        // posts.push({...ctx.request.body, id: nextId++, created: Date.now()});
+        if (id !== 0) {
+            posts = posts.map(o => o.id !== id ? o : {...o, content: content});
+            ctx.response.status = 204;
+            return;
+        }
+
+        posts.push({...ctx.request.body, id: nextId++, created: Date.now()});
         ctx.response.status = 204;
 })
 
     .delete('/posts/:id', async(ctx, next) => {
         const postId = Number(ctx.params.id);
 
-        await Post.delete(postId);
-        // const index = posts.findIndex(o => o.id === postId);
-        // if (index !== -1) {
-        //     posts.splice(index, 1);
-        // }
+        // await Post.delete(postId);
+        const index = posts.findIndex(o => o.id === postId);
+        if (index !== -1) {
+            posts.splice(index, 1);
+        }
         ctx.response.body = 'deleted';
         ctx.response.status = 204;
 });
